Emresav/ROSPlan

commit 6840478eacb4ead59f20c7f394bbee899f3c00ec

Fixed bug in parser when action has no parameters

rosplan_planning_system/src/PlanParser.cpp
@@ -69,27 +69,42 @@ namespace KCL_rosplan {
 					double dispatchTime = (double)atof(line.substr(0,curr).c_str());
 					msg.dispatch_time = dispatchTime;
 
-					// name
+					// check for parameters
 					curr=line.find("(")+1;
-					next=line.find(" ",curr);
-					std::string name = line.substr(curr,next-curr).c_str();
-					msg.name = name;
+					bool paramsExist = (line.find(" ",curr) > line.find(")",curr));
+					
+					if(paramsExist) {
 
-					// parameters
-					std::vector<std::string> params;
-					curr=next+1;
-					next=line.find(")",curr);
-					int at = curr;
-					while(at < next) {
-						int cc = line.find(" ",curr);
-						int cc1 = line.find(")",curr);
-						curr = cc<cc1?cc:cc1;
-						std::string param = environment.name_map[line.substr(at,curr-at)];
-						params.push_back(param);
-						++curr;
-						at = curr;
+						// name
+						next=line.find(" ",curr);
+						std::string name = line.substr(curr,next-curr).c_str();
+						msg.name = name;
+
+						// parameters
+						std::vector<std::string> params;
+						curr=next+1;
+						next=line.find(")",curr);
+						int at = curr;
+						while(at < next) {
+							int cc = line.find(" ",curr);
+							int cc1 = line.find(")",curr);
+							curr = cc<cc1?cc:cc1;
+							std::string param = environment.name_map[line.substr(at,curr-at)];
+							params.push_back(param);
+							++curr;
+							at = curr;
+						}
+						processPDDLParameters(msg, params, environment);
+
+
+					} else {
+
+						// name
+						next=line.find(")",curr);
+						std::string name = line.substr(curr,next-curr).c_str();
+						msg.name = name;
+
 					}
-					processPDDLParameters(msg, params, environment);
 
 					// duration
 					curr=line.find("[",curr)+1;

commit a95b745d0e9f429940e1f412a3e23bededba8f64

Fix little bug introduced in 6840478eacb4ead59f20c7f394bbee899f3c00ec

rosplan_planning_system/src/PlanParser.cpp
@@ -71,8 +71,8 @@ namespace KCL_rosplan {
 
 					// check for parameters
 					curr=line.find("(")+1;
-					bool paramsExist = (line.find(" ",curr) > line.find(")",curr));
-					
+					bool paramsExist = (line.find(" ",curr) < line.find(")",curr));
+
 					if(paramsExist) {
 
 						// name